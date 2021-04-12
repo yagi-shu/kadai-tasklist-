7e889a712bf9b65d2e7a5f8db394ca7a2f9cfd6a
yagi.shu <yagishu93@gmail.com>
2021-04-12 02:36:26 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/">tasks</a>
+        <a class="navbar-brand" href="/">taskslist</a>
 
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -9,11 +9,11 @@
 
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
+            <ul class="nav navbar-nav navbar-right">
                 {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
                 {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li><a href="#">Login</a></li>
             </ul>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 /*
 Route::get('/', 'TasksController@index');
 
